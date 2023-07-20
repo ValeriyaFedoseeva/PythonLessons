e61d23ce93cc2004d4554c0bed615586e9ec0b40
Valeriya Fedoseeva <136153985+ValeriyaFedoseeva@users.noreply.github.com>
2023-07-20 22:20:23 +0300

    Update Qt_UI.py

--- Lesson6/Qt_UI.py
+++ Lesson6/Qt_UI.py
@@ -1,13 +1,21 @@
 import maya.cmds as cmds
 from PySide2 import QtWidgets, QtCore, QtGui
 
+if cmds.window("MyQtUI", exists=True):
+    cmds.deleteUI("MyQtUI", window=True)
+
+if cmds.windowPref("MyQtUI", exists=True):
+    cmds.windowPref("MyQtUI", remove=True)
 
 class MyWindow(QtWidgets.QDialog):
 
-    def __init__(self):
+    def __init__(self, parent=None):
 
         super(MyWindow, self).__init__()
+        
+        self.setup_UI()
 
+    def setup_UI(self):
         self.setWindowTitle("Create Poly Primitives")
         self.setMinimumSize(300, 200)
         self.setMaximumSize(600, 800)
@@ -26,8 +34,8 @@ class MyWindow(QtWidgets.QDialog):
         self.check_box_layout = QtWidgets.QVBoxLayout()
         self.main_layout.addLayout(self.check_box_layout)
 
-        self.radio_btn_xyz_layout = QtWidgets.QHBoxLayout()
-        self.main_layout.addLayout(self.radio_btn_xyz_layout)
+        self.chk_btn_xyz_layout = QtWidgets.QHBoxLayout()
+        self.main_layout.addLayout(self.chk_btn_xyz_layout)
 
         self.slider_layout = QtWidgets.QHBoxLayout()
         self.main_layout.addLayout(self.slider_layout)
@@ -35,42 +43,15 @@ class MyWindow(QtWidgets.QDialog):
         self.btn_layout = QtWidgets.QHBoxLayout()
         self.main_layout.addLayout(self.btn_layout)
 
-        # Text Line
-        self.nameLabel = QtWidgets.QLabel(self)
-        self.nameLabel.setText('Name:')
-        self.text_field_layout.addWidget(self.nameLabel)
-        self.saveNameField = QtWidgets.QLineEdit()
-        self.text_field_layout.addWidget(self.saveNameField)
 
-        # Radio Buttons
-        self.rbutton_sphere = QtWidgets.QRadioButton("Sphere")
-        self.rbutton_sphere.setChecked(True)
-        self.rbutton_cube = QtWidgets.QRadioButton("Cube")
-        self.rbutton_cone = QtWidgets.QRadioButton("Cone")
-
-        self.radio_btn_layout.addWidget(self.rbutton_sphere)
-        self.radio_btn_layout.addWidget(self.rbutton_cube)
-        self.radio_btn_layout.addWidget(self.rbutton_cone)
-
-        # Check box
-        self.chk_box_group = QtWidgets.QCheckBox("Group")
-        self.chk_box_layer = QtWidgets.QCheckBox("Display Layer")
-        self.chk_box_offset = QtWidgets.QCheckBox("Offset in:")
-
-        self.check_box_layout.addWidget(self.chk_box_group)
-        self.check_box_layout.addWidget(self.chk_box_layer)
-        self.check_box_layout.addWidget(self.chk_box_offset)
-
-        # Radio Buttons
-        self.rbutton_X = QtWidgets.QCheckBox("X")
-        self.rbutton_X.setChecked(True)
-        self.rbutton_Y = QtWidgets.QCheckBox("Y")
-        self.rbutton_Z = QtWidgets.QCheckBox("Z")
-
-        self.radio_btn_xyz_layout.addWidget(self.rbutton_X)
-        self.radio_btn_xyz_layout.addWidget(self.rbutton_Y)
-        self.radio_btn_xyz_layout.addWidget(self.rbutton_Z)
-
+        #UI content
+        self.create_top_textfield()
+        self.create_upper_radiobuttons()
+        self.create_checkboxes()
+        self.create_lower_radiobuttons()
+        #self.create_slider()
+        self.create_action_buttons()
+        
         # Slider
         self.slider = QtWidgets.QSlider()
         self.slider_layout.addWidget(self.slider)
@@ -83,40 +64,150 @@ class MyWindow(QtWidgets.QDialog):
         self.show_set_number = QtWidgets.QLineEdit()
         self.slider_layout.addWidget(self.show_set_number)
         self.show_set_number.setMaximumWidth(50)
+        
+        
+        default_value = 0  
+        self.slider.setValue(default_value)
+        self.update_line_edit(default_value)
+        self.slider.valueChanged.connect(self.update_line_edit)
 
+    def update_line_edit(self, value):
+        self.show_set_number.setText(str(value))       
+
+    def create_top_textfield(self):
+        # Text Line
+        self.nameLabel = QtWidgets.QLabel(self)
+        self.nameLabel.setText('Name:')
+        self.text_field_layout.addWidget(self.nameLabel)
+        self.saveNameField = QtWidgets.QLineEdit()
+        self.text_field_layout.addWidget(self.saveNameField)
+
+    def create_upper_radiobuttons(self):
+        # Radio Buttons
+        self.rbutton_sphere = QtWidgets.QRadioButton("Sphere")
+        self.rbutton_sphere.setChecked(True)
+        self.rbutton_cube = QtWidgets.QRadioButton("Cube")
+        self.rbutton_cone = QtWidgets.QRadioButton("Cone")
+
+        self.radio_btn_layout.addWidget(self.rbutton_sphere)
+        self.radio_btn_layout.addWidget(self.rbutton_cube)
+        self.radio_btn_layout.addWidget(self.rbutton_cone)
+
+    def create_checkboxes(self):
+        # Check box
+        self.chk_box_group = QtWidgets.QCheckBox("Group")
+        self.chk_box_layer = QtWidgets.QCheckBox("Display Layer")
+        self.chk_box_offset = QtWidgets.QCheckBox("Offset in:")
+
+        self.check_box_layout.addWidget(self.chk_box_group)
+        self.check_box_layout.addWidget(self.chk_box_layer)
+        self.check_box_layout.addWidget(self.chk_box_offset)
+
+
+    def create_lower_radiobuttons(self):
+        # Radio Buttons
+        self.chk_button_X = QtWidgets.QCheckBox("X")
+        self.chk_button_X.setChecked(True)
+        self.chk_button_Y = QtWidgets.QCheckBox("Y")
+        self.chk_button_Z = QtWidgets.QCheckBox("Z")
+
+        self.chk_btn_xyz_layout.addWidget(self.chk_button_X)
+        self.chk_btn_xyz_layout.addWidget(self.chk_button_Y)
+        self.chk_btn_xyz_layout.addWidget(self.chk_button_Z)
+
+
+
+    def create_action_buttons(self):
         # Buttons
-        self.button_OK = QtWidgets.QPushButton("OK")
-        self.button_OK.clicked.connect(self.on_button_OK_clicked)
+        self.button_create = QtWidgets.QPushButton("Create")
+        self.button_create.clicked.connect(self.on_button_create_clicked)
 
-        self.button_Apply = QtWidgets.QPushButton("Apply")
-        self.button_Apply.clicked.connect(self.on_button_Apply_clicked)
+        self.button_apply = QtWidgets.QPushButton("Apply")
+        self.button_apply.clicked.connect(self.on_button_apply_clicked)
 
-        self.button_Cancel = QtWidgets.QPushButton("Close")
-        self.button_Cancel.clicked.connect(self.on_button_Close_clicked)
+        self.button_close = QtWidgets.QPushButton("Close")
+        self.button_close.clicked.connect(self.on_button_close_clicked)
 
-        self.btn_layout.addWidget(self.button_OK)
-        self.btn_layout.addWidget(self.button_Apply)
-        self.btn_layout.addWidget(self.button_Cancel)
+        self.btn_layout.addWidget(self.button_create)
+        self.btn_layout.addWidget(self.button_apply)
+        self.btn_layout.addWidget(self.button_close)
 
-    def on_button_OK_clicked(self):
+
+    def operation(self):
+        newName = cmds.textField("ItemName", q=True, text=True)
+
+        radio_selection = cmds.radioCollection(radioCollection, query=True, sl=True)
+
+
+    def on_button_create_clicked(self):
+        suffix = '_Geo'
+        gp = '_Grp'
+        lyr = '_Layer'
+        name = self.saveNameField.text()
+    
         if self.rbutton_sphere.isChecked():
-            cmds.polySphere()
+            obj = cmds.polySphere(name='{}{}'.format(name, suffix))[0]
+            if self.chk_box_group.isChecked():
+                grp = cmds.group(obj, n="{}{}".format(name, gp))
+                if self.chk_box_layer.isChecked():
+                    layer = cmds.createDisplayLayer(n="{}{}".format(name, lyr))
+                    cmds.editDisplayLayerMembers(layer, obj)
+    
+            if self.chk_box_offset.isChecked():
+                offset = float(self.show_set_number.text())
+                if self.chk_button_X.isChecked():
+                    cmds.move(offset, 0, 0, obj, r=True)
+                if self.chk_button_Y.isChecked():
+                    cmds.move(0, offset, 0, obj, r=True)
+                if self.chk_button_Z.isChecked():
+                    cmds.move(0, 0, offset, obj, r=True)
+    
+        elif self.rbutton_cube.isChecked():
+            obj = cmds.polyCube(name='{}{}'.format(name, suffix))[0]
+            if self.chk_box_group.isChecked():
+                grp = cmds.group(obj, n="{}{}".format(name, gp))
+                if self.chk_box_layer.isChecked():
+                    layer = cmds.createDisplayLayer(n="{}{}".format(name, lyr))
+                    cmds.editDisplayLayerMembers(layer, obj)
+    
+            if self.chk_box_offset.isChecked():
+                offset = float(self.show_set_number.text())
+                if self.chk_button_X.isChecked():
+                    cmds.move(offset, 0, 0, obj, r=True)
+                if self.chk_button_Y.isChecked():
+                    cmds.move(0, offset, 0, obj, r=True)
+                if self.chk_button_Z.isChecked():
+                    cmds.move(0, 0, offset, obj, r=True)
+    
+        elif self.rbutton_cone.isChecked():
+            obj = cmds.polyCone(name='{}{}'.format(name, suffix))[0]
+            if self.chk_box_group.isChecked():
+                grp = cmds.group(obj, n="{}{}".format(name, gp))
+                if self.chk_box_layer.isChecked():
+                    layer = cmds.createDisplayLayer(n="{}{}".format(name, lyr))
+                    cmds.editDisplayLayerMembers(layer, obj)
+    
+            if self.chk_box_offset.isChecked():
+                offset = float(self.show_set_number.text())
+                if self.chk_button_X.isChecked():
+                    cmds.move(offset, 0, 0, obj, r=True)
+                if self.chk_button_Y.isChecked():
+                    cmds.move(0, offset, 0, obj, r=True)
+                if self.chk_button_Z.isChecked():
+                    cmds.move(0, 0, offset, obj, r=True)
 
-    def on_button_Apply_clicked(self):
+
+    def on_button_apply_clicked(self):
         print("Apply is clicked")
+        
 
-    def on_button_Close_clicked(self):
+    def on_button_close_clicked(self):
         print("Close is clicked")
-        if cmds.window("MyQtUI", query=True, exists=True):
-            cmds.deleteUI('MyQtUI')
-        if cmds.windowPref("MyQtUI", exists=True):
-            cmds.windowPref("MyQtUI", remove=1)
+        self.close()
 
 
-if cmds.window("MyQtUI", query=True, exists=True):
-    cmds.deleteUI('MyQtUI')
-if cmds.windowPref("MyQtUI", exists=True):
-    cmds.windowPref("MyQtUI", remove=1)
+
+
 
 win = MyWindow()
 win.show()
