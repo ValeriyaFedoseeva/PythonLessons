9c52190d46b109f21f119daaec719936cd5a61d0
Valeriya Fedoseeva <136153985+ValeriyaFedoseeva@users.noreply.github.com>
2023-07-28 01:18:24 +0300

    Update mySelectionSet.py

--- Lesson7/mySelectionSet.py
+++ Lesson7/mySelectionSet.py
@@ -3,40 +3,115 @@ import random
 from PySide2 import QtCore, QtGui, QtWidgets
 
 
-class ObjectWidget(QtWidgets.QWidget):
-    def __init__(self, object_path):
-        super(ObjectWidget, self).__init__()
 
-        self.object_path = object_path
-        self.display_name = self.object_path.split("|")[-1]
+class Selection_Set_Widget(QtWidgets.QWidget):
+    def __init__(self, set_count):
+        super(Selection_Set_Widget, self).__init__()
 
-        self.random_color = self.generate_random_color()
+        self.get_selected()
+
+        self.setMinimumWidth(275)
+        self.setMinimumHeight(50)
+        self.setMaximumHeight(50)
+        self.setAutoFillBackground(True)
+
+        self.set_count = set_count 
+
+        self.set_background()
 
         self.setup_ui()
 
+        # Context menu
+
+        self.popMenu = QtWidgets.QMenu(self)
+
+        self.popMenuAdd = QtWidgets.QAction('Add selected object to set', self)
+        self.popMenu.addAction(self.popMenuAdd)
+        self.popMenuAdd.triggered.connect(self.cont_add_obj)
+
+        self.popMenuDel = QtWidgets.QAction('Delete selected object from set', self)
+        self.popMenu.addAction(self.popMenuDel)
+        self.popMenuDel.triggered.connect(self.cont_del_obj)   
+
+        self.popMenuDel = QtWidgets.QAction('Select all objects in this set', self)
+        self.popMenu.addAction(self.popMenuDel)
+        self.popMenuDel.triggered.connect(self.cont_sel_obj)         
+
+        self.popMenuDel = QtWidgets.QAction('Delete set', self)
+        self.popMenu.addAction(self.popMenuDel)
+        self.popMenuDel.triggered.connect(self.cont_del_set)   
+
+        # attributes
+        self.setMouseTracking(True)  
+        self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.onContextMenu)
+
+
+    def onContextMenu(self, point):
+
+        self.popMenu.exec_(self.mapToGlobal(point))
+
+
+    def cont_add_obj(self):
+        sel = cmds.ls(sl=True, l=True)
+        if not sel:
+            raise RuntimeError("No objects selected. Please select some objects.")
+ 
+        for obj in sel:
+            if obj not in self.selection:
+                self.selection.append(obj)
+
+    def cont_del_obj(self):
+        sel = cmds.ls(sl=True, l=True)
+        if not sel:
+            raise RuntimeError("No objects selected. Please select some objects.")
+        
+        for obj in sel:
+            if obj in self.selection:
+                self.selection.remove(obj)
+
+    def cont_sel_obj(self):
+        cmds.select(self.selection)     
+
+    def cont_del_set(self):
+        self.deleteLater()
+
+
     def setup_ui(self):
-        self.setMinimumSize(250, 50)
-        self.setMaximumHeight(50)
-
-        self.setAutoFillBackground(True)
-        self.set_background()
-
         # layout 
         self.main_layout = QtWidgets.QHBoxLayout()
         self.setLayout(self.main_layout)
 
+        #Naming
+        self.lable = QtWidgets.QLabel("SET {}".format(self.set_count))
+        self.font = self.lable.font()
+        self.font.setPointSize(10)
+        self.font.setBold(True)
+        self.lable.setFont(self.font)
+        self.main_layout.addWidget(self.lable)
+        
 
-    def generate_random_color(self):
-        hue = random.uniform(0.0, 1.0)
-        color = QtGui.QColor.fromHsvF(hue, 1.0, 1.0)
-        return color
+    def get_selected(self):
+        self.selection = cmds.ls(sl=1, l=1)
+        return self.selection
+
+    def selectObjects(self):
+        cmds.ls(self.selection)
 
 
-    def set_background(self):
-        palette = self.palette()
-        palette.setColor(QtGui.QPalette.Background, self.random_color)
-        self.setPalette(palette)
-    
+    def set_background(self, r=None, g=75, b=150):
+        #set Background
+
+        if r is None:
+            r = random.randint(0, 255)
+
+        self.p = QtGui.QPalette()
+        self.color = QtGui.QColor()
+        self.color.setHsv(r, g, b)
+        self.p.setColor(self.backgroundRole(), self.color)
+        self.setPalette(self.p)
+
+
     def enterEvent(self, event):
         self.setCursor(QtCore.Qt.PointingHandCursor)
         self.set_background(160, 25, 150)
@@ -49,17 +124,24 @@ class ObjectWidget(QtWidgets.QWidget):
         self.set_background(160, 10, 150)
 
     def mouseReleaseEvent(self, event):
-        #self.set_background(160, 50, 150)    
-        state = self.checkbox1.isChecked()
+        self.set_background(160, 25, 150)
 
+        if event.button() == QtCore.Qt.LeftButton:
+            cmds.select(self.selection)  # Call get_selected method to record selected objects
+
+        elif event.button() == QtCore.Qt.RightButton:
+            self.onContextMenu(event.pos())
+            
 
 class MyCustomWidget(QtWidgets.QDialog):
     def __init__(self):
         super(MyCustomWidget, self).__init__()
 
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+
         self.selection = []
 
-
+        self.set_count = 0
         self.setup_ui()
 
     def setup_ui(self):
@@ -69,15 +151,14 @@ class MyCustomWidget(QtWidgets.QDialog):
         
         self.main_layout = QtWidgets.QVBoxLayout()
         self.main_layout.setAlignment(QtCore.Qt.AlignTop)
-        self.main_layout.setContentsMargins(5, 5, 5, 5)
+        self.main_layout.setContentsMargins(3, 3, 3, 3)
         self.main_layout.setSpacing(5)
         self.setLayout(self.main_layout)
 
 
-  
         # scroll area
         self.scrollArea = QtWidgets.QScrollArea()
-        self.scrollArea.setMinimumHeight(490)
+        self.scrollArea.setMinimumHeight(500)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setMinimumWidth(300)
         self.scrollArea.setFocusPolicy( QtCore.Qt.NoFocus )
@@ -88,7 +169,7 @@ class MyCustomWidget(QtWidgets.QDialog):
 
         self.scroll_layout = QtWidgets.QVBoxLayout()
         self.scroll_layout.setAlignment(QtCore.Qt.AlignTop)
-        self.scroll_layout.setContentsMargins(0,0,5,0)
+        self.scroll_layout.setContentsMargins(3,0,3,0)
         self.scroll_layout.setSpacing(5) #layout
         self.scroll_area_widget.setLayout(self.scroll_layout)
 
@@ -96,12 +177,17 @@ class MyCustomWidget(QtWidgets.QDialog):
 
 
         self.btn_new = QtWidgets.QPushButton("Create New Set")
+        self.font = self.btn_new.font()
+        self.font.setPointSize(15)
+        self.btn_new.setFont(self.font)
         self.btn_new.setMinimumHeight(80)
-        self.btn_new.clicked.connect(self.on_button_new_clicked)
+        self.btn_new.clicked.connect(self.create_new_selection_set)
         self.main_layout.addWidget(self.btn_new)
 
-    def on_button_new_clicked(self):
-        pass
+    def create_new_selection_set(self):
+        self.set_count += 1  
+        btn = Selection_Set_Widget(self.set_count)
+        self.scroll_layout.addWidget(btn)
 
 
 def clean_ui():
